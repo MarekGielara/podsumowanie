d97a0087c199945d8e4eb8da4abe352832fce632
Witaj Admin <marekgielara22@poczta.onet.pl>
2018-12-08 12:35:58 +0100

    Podsumowanie projektu1

---
+++ src/test/java/pl/iait/podsumowanie/Init.java
@@ -0,0 +1,20 @@
+package pl.iait.podsumowanie;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Init
+{
+	static WebDriver driver;
+
+	public static WebDriver getDriver()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Tester\\CHD\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.get("https://www.seleniumhq.org/");
+
+		return driver;
+
+	}
+}
---
+++ src/test/java/pl/iait/podsumowanie/TopMenuTest.java
@@ -0,0 +1,17 @@
+package pl.iait.podsumowanie;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TopMenuTest
+{
+
+	@Test
+	public void topMenu()
+	{
+		WebDriver driver = Init.getDriver();
+		driver.findElement(By.linkText("About")).click();
+
+	}
+}
